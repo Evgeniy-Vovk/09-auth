3ef8d31fafa1c9e74815cba9b0f3b4fa65799c73
Evgeniy Vovk <evgeniy.vovk@ukr.net>
2026-05-27 19:17:59 +0300

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "ac.goit.global",
-                port: "",
-                pathname: "/**",
-            },
-        ],
-    },
+  /* config options here */
 };
 
 export default nextConfig;
